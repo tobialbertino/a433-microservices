f3c76d7337ac6b372500f98cc8e98e673bf38818
Tobi Albertino <muhamad_tobiaja232@apps.ipb.ac.id>
2022-11-21 01:18:13 +0700

    add sh file

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# kriteria 3:
+# membuat Docker image dari Dockerfile
+docker build -t karsa-jobs-be:v1 .
+
+# Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+
+# docker image tag item-app:v1 tobialbertino/item-app:v1
+docker image tag item-app:v1 ghcr.io/tobialbertino/karsa-jobs-be:v1
+
+# Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal. 
+export PASSWORD_DOCKER_HUB=token
+echo $PASSWORD_DOCKER_HUB | docker login ghcr.io -u tobialbertino --password-stdin
+# docker login -u tobialbertino -p $1
+
+# push image ke docker hub / ghcr
+# docker push tobialbertino/item-app:v1
+docker push ghcr.io/tobialbertino/karsa-jobs-be:v1
+
+# TODO: Edit lagi
